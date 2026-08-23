12908e9ba227c27a95db60fd38d38657f1e99698
mn625237-cyber <mn625237@gmail.com>
2026-08-24 01:20:13 +0300

    Update extract-phone.js

--- api/extract-phone.js
+++ api/extract-phone.js
@@ -38,8 +38,7 @@ module.exports = async function handler(req, res) {
             ],
           }],
           generationConfig: {
-            temperature: 0,
-            maxOutputTokens: 200,
+            maxOutputTokens: 2048,
           },
         }),
       }
